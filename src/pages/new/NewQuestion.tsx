3065971b9eb6e5cc1e7c4f775867eb87de15f890
Artem Konkov <kxnkxv@yandex.ru>
2021-09-30 19:10:02 +0300

    example page

--- src/pages/new/NewQuestion.tsx
+++ src/pages/new/NewQuestion.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import {Button, Form, Input, Typography} from 'antd';
 
 const NewQuestion = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div>
-      NewQuestion
-    </div>
+    <Form
+      name="basic"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Typography.Title>Создать вопрос</Typography.Title>
+      <Form.Item>
+        <Input.TextArea/>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Создать вопрос
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
