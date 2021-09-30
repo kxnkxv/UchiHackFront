247178ef76d77db725fb6a77b46e9fa874ae5d50
Artem Konkov <kxnkxv@yandex.ru>
2021-09-30 19:15:34 +0300

    Auth

--- src/pages/authorization/Authorization.tsx
+++ src/pages/authorization/Authorization.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import {Button, Card, Checkbox, Form, Input} from 'antd';
 import Auth from "../../store/auth";
-import {Button} from "antd";
 
 const Authorization = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div>
-      Authorization
-      <Button onClick={() => Auth.setIsUserAuth(true)}>войти</Button>
-    </div>
+    <Card>
+      <Form
+        name="Authorization"
+        initialValues={{remember: true}}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{required: true, message: 'Please input your username!'}]}
+        >
+          <Input/>
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{required: true, message: 'Please input your password!'}]}
+        >
+          <Input.Password/>
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked" wrapperCol={{offset: 8, span: 16}}>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{offset: 8, span: 16}}>
+          <Button type="primary" htmlType="submit" onClick={() => Auth.setIsUserAuth(true)}>
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+
   );
 };
 
