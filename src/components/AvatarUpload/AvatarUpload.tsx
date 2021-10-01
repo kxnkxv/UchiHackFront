78d4013d5697c03b768374b3d5e5d94447540445
Artem Konkov <kxnkxv@yandex.ru>
2021-10-01 14:57:17 +0300

    uploaderCrop

--- src/components/AvatarUpload/AvatarUpload.tsx
+++ src/components/AvatarUpload/AvatarUpload.tsx
@@ -1,74 +1,54 @@
-import {message, Upload} from 'antd';
-import {LoadingOutlined, PlusOutlined} from '@ant-design/icons';
-import {Component} from 'react';
+import React, {useState} from 'react';
+import {Upload} from 'antd';
+import ImgCrop from 'antd-img-crop';
+import User from "./../../store/user/user"
 
-// @ts-ignore
-function getBase64(img, callback) {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result));
-  reader.readAsDataURL(img);
-}
+const AvatarUpload = () => {
+  const [fileList, setFileList] = useState([
+    {
+      uid: '1',
+      name: 'avatar.png',
+      status: 'done',
+      url: User.user.avatar,
+    },
+  ]);
 
-// @ts-ignore
-function beforeUpload(file) {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-  if (!isJpgOrPng) {
-    message.error('You can only upload JPG/PNG file!');
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-  return isJpgOrPng && isLt2M;
-}
-
-// @ts-ignore
-class AvatarUpload extends Component {
-  state = {
-    loading: false,
+  // @ts-ignore
+  const onChange = ({fileList: newFileList}) => {
+    setFileList(newFileList);
   };
 
-// @ts-ignore
-  handleChange = info => {
-    if (info.file.status === 'uploading') {
-      this.setState({loading: true});
-      return;
+  // @ts-ignore
+  const onPreview = async file => {
+    let src = file.url;
+    if (!src) {
+      src = await new Promise(resolve => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+        reader.onload = () => resolve(reader.result);
+      });
     }
-    if (info.file.status === 'done') {
-      // Get this url from response in real world.
-      // @ts-ignore
-      getBase64(info.file.originFileObj, imageUrl =>
-        this.setState({
-          imageUrl,
-          loading: false,
-        }),
-      );
-    }
-  };
-
-  render() {
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
     // @ts-ignore
-    const {loading, imageUrl} = this.state;
-    const uploadButton = (
-      <div>
-        {loading ? <LoadingOutlined/> : <PlusOutlined/>}
-        <div style={{marginTop: 8}}>Upload</div>
-      </div>
-    );
-    return (
+    imgWindow.document.write(image.outerHTML);
+  };
+
+  return (
+    <ImgCrop rotate>
       <Upload
-        name="avatar"
-        listType="picture-card"
-        className="avatar-uploader"
-        showUploadList={false}
         action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        beforeUpload={beforeUpload}
-        onChange={this.handleChange}
+        listType="picture-card"
+        // @ts-ignore
+        fileList={fileList}
+        onChange={onChange}
+        onPreview={onPreview}
       >
-        {imageUrl ? <img src={imageUrl} alt="avatar" style={{width: '100%'}}/> : uploadButton}
+        {fileList.length < 5 && '+ Upload'}
       </Upload>
-    );
-  }
-}
+    </ImgCrop>
+  );
+};
 
 export default AvatarUpload;
