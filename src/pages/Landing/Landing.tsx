4d8fb2327ddc98715195fcd47f619d69b67ff3dc
Artem Konkov <kxnkxv@yandex.ru>
2021-10-01 18:01:04 +0300

    Ого, это что? Лендинг? Вау

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {I18nextProvider} from "react-i18next";
+import Router from "./router";
+import i18n from './translation';
+
+const Landing = () => {
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Router />
+    </I18nextProvider>
+  );
+};
+
+export default Landing;
