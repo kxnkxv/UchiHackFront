231f3e0ccb5c5d8334063c805a03a00ed2c3fbcb
Artem Konkov <kxnkxv@yandex.ru>
2021-10-01 17:03:11 +0300

    Revert "Лендинг"
    This reverts commit 5e4a4ce329a0e17b5abbcb92ce2612ee017a7374.

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,27 +1,27 @@
 import {FC} from 'react';
-import {Redirect, Route, Switch,} from "react-router-dom";
+import {Link, Redirect, Route, Switch,} from "react-router-dom";
 import {observer} from "mobx-react";
 import {AUTH, REGISTRATION, RESTORE_PASSWORD} from "../constants/routes";
 import Auth from '../store/auth'
 import Registration from "../pages/Registration/Registration"
 import Authorization from "../pages/Authorization/Authorization"
 import PrivateRoutes from "./PrivateRoutes";
+import {Button} from "antd";
 import RestorePassword from '../pages/RestorePassword/RestorePassword';
-import Landing from "../pages/Landing/Landing";
 
 const Routes: FC = () => {
 
   return Auth.isUserAuth ? <PrivateRoutes/> : (
     <Switch>
-      <Route exact path="/" component={Landing}/>
-      {/*  <p>Лендинг</p>*/}
-      {/*  <Link to={AUTH}>*/}
-      {/*    <Button type="primary">Войти</Button>*/}
-      {/*  </Link>*/}
-      {/*  <Link to={REGISTRATION}>*/}
-      {/*    <Button>Зарегистрироваться</Button>*/}
-      {/*  </Link>*/}
-      {/*</Route>*/}
+      <Route exact path="/">
+        <p>Лендинг</p>
+        <Link to={AUTH}>
+          <Button type="primary">Войти</Button>
+        </Link>
+        <Link to={REGISTRATION}>
+          <Button>Зарегистрироваться</Button>
+        </Link>
+      </Route>
       <Route exact path={RESTORE_PASSWORD} component={RestorePassword}/>
       <Route exact path={AUTH} component={Authorization}/>
       <Route exact path={REGISTRATION} component={Registration}/>
