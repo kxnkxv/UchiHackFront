b8e8e4a2f568a548dbc1e44d911fb2219a57f947
Artem Konkov <kxnkxv@yandex.ru>
2021-10-02 03:11:54 +0300

    Попытка вставить локаль номер 268, кажется я уже перестал чувствовать свой мозг

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import Routes from "./router/Routes";
 import moment from "moment";
 import "moment/locale/ru";
 
 const App: FC = () => {
-  moment.locale("ru");
+  useEffect(() => {
+    moment.locale("ru");
+  }, []);
   return <Routes />;
 };
 
