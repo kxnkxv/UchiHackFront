c80acc3ae7aca7764df011892ed85851430eaf82
Artem Konkov <kxnkxv@yandex.ru>
2021-10-02 15:21:20 +0300

    Allthemes page, заглушка если вопросы отсутствуют

--- src/pages/Themes/Themes.tsx
+++ src/pages/Themes/Themes.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
-import { RouteComponentProps } from "react-router-dom";
+import { Link, RouteComponentProps } from "react-router-dom";
 import { questionsList } from "../../mockData/questions";
 import ThemeItem from "./components/ThemeItem/ThemeItem";
-import { Row } from "antd";
+import { Button, Col, Empty, Image, Row, Space } from "antd";
 
-import { Container, Heading, List } from "./styled";
+import { Container, Heading, List, Theme } from "./styled";
+import { questionsThemes } from "../../constants/questionsThemes";
+import { NEW_QUESTION, THEMES } from "../../constants/routes";
 
 interface RouteParams {
   themeId: string;
@@ -12,21 +14,54 @@ interface RouteParams {
 
 const Themes: FC<RouteComponentProps<RouteParams>> = ({ match }) => {
   const { themeId } = match.params;
-  return (
-    <Container>
-      <Row align="middle">
-        <Heading>Все вопросы</Heading>
+  if (themeId === "allThemes") {
+    return (
+      <Row align="middle" justify="space-around">
+        <Col>
+          <Space wrap size={[25, 25]}>
+            {Object.keys(questionsThemes).map((key) => (
+              <Link to={`${THEMES}/${key}`} key={key}>
+                <Theme>
+                  <Image src="https://picsum.photos/150/100" preview={false} />
+                  {questionsThemes[key]}
+                </Theme>
+              </Link>
+            ))}
+          </Space>
+        </Col>
       </Row>
-      <List>
-        {questionsList.map(
-          (question) =>
-            (!themeId || themeId === question.theme) && (
+    );
+  } else {
+    const questions = questionsList.filter(
+      (question) => !themeId || themeId === question.theme
+    );
+    if (questions.length !== 0) {
+      return (
+        <Container>
+          <Row align="middle">
+            <Heading>Все вопросы</Heading>
+          </Row>
+          <List>
+            {questions.map((question) => (
               <ThemeItem question={question} />
-            )
-        )}
-      </List>
-    </Container>
-  );
+            ))}
+          </List>
+        </Container>
+      );
+    } else {
+      return (
+        <Empty
+          description={
+            <span>Пока в этом разделе ещё никто-ничего не спрашивал</span>
+          }
+        >
+          <Link to={NEW_QUESTION}>
+            <Button type="primary">Я буду первым!</Button>
+          </Link>
+        </Empty>
+      );
+    }
+  }
 };
 
 export default Themes;
