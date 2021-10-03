8d605ef372af71616570c37c4fd9f7479e87ba1f
Artem Konkov <kxnkxv@yandex.ru>
2021-10-03 05:15:09 +0300

    api

--- src/pages/Profile/PublicProfile/PublicProfile.tsx
+++ src/pages/Profile/PublicProfile/PublicProfile.tsx
@@ -1,15 +1,12 @@
-import { Avatar, Button, Card, Col, Row, Tabs } from "antd";
+import { Avatar, Button, Card, Col, Empty, Row, Tabs } from "antd";
 import React, { FC, useEffect, useState } from "react";
-// import { users } from "../../../mockData/users";
 import { UserType } from "../../../types/UserType";
 import { UserOutlined } from "@ant-design/icons";
-// import { questionsList } from "../../../mockData/questions";
 import ThemeItem from "../../Themes/components/ThemeItem/ThemeItem";
 import { List } from "../../Themes/styled";
 import NotFound from "../../NotFound/NotFound";
 import { QuestionType } from "../../../types/QuestionType";
 import { AnswerType } from "../../../types/AnswerType";
-import Answer from "../../Question/components/Answer";
 import axios from "axios";
 import { URL } from "../../../constants/API";
 import Auth from "../../../store/auth";
@@ -24,7 +21,9 @@ const PublicProfile: FC<PublicProfileProps> = ({ userId }) => {
   // @ts-ignore
   const [currentUser, setCurrentUser] = useState<UserType>(null);
   const [questions, setQuestions] = useState<QuestionType[]>([]);
-  const [answers, setAnswers] = useState<AnswerType[]>([]);
+
+  const answers = [];
+  const questionsId: string[] = [];
 
   const getUserById = (id: string) => {
     axios
@@ -38,9 +37,39 @@ const PublicProfile: FC<PublicProfileProps> = ({ userId }) => {
       .then((r) => setCurrentUser(r.data));
   };
 
+  const getQuestionsByUserId = (id: string) => {
+    axios
+      .request<QuestionType[]>({
+        method: "get",
+        url: `${URL}/questions/`,
+        headers: {
+          Authorization: `Bearer ${Auth.token.accessToken}`,
+        },
+      })
+      .then((r) => {
+        const filtered = r.data.filter((question) => id !== question.user.id);
+        setQuestions(filtered);
+      });
+  };
+
+  const getAnswersByUserId = () => {
+    questionsId.map((id) => {
+      axios
+        .request<AnswerType>({
+          method: "get",
+          url: `${URL}/answers/question/${id}/`,
+          headers: {
+            Authorization: `Bearer ${Auth.token.accessToken}`,
+          },
+        })
+        .then((r) => answers.push(r.data));
+    });
+  };
+
   useEffect(() => {
-    //  TODO: получить список вопросов (и отдельно ответов) + юзера
     getUserById(userId);
+    getQuestionsByUserId(userId);
+    getAnswersByUserId();
   }, []);
   return currentUser ? (
     <Card>
@@ -70,19 +99,22 @@ const PublicProfile: FC<PublicProfileProps> = ({ userId }) => {
         <Tabs defaultActiveKey="1">
           <TabPane tab="Вопросы" key="1">
             <List>
-              {questions
-                ? questions.map((question: QuestionType) => (
-                    <ThemeItem question={question} />
-                  ))
-                : null}
+              {questions.length === 0 ? (
+                questions.map((question: QuestionType) => {
+                  questionsId.push(question.id);
+                  return <ThemeItem question={question} />;
+                })
+              ) : (
+                <Empty />
+              )}
             </List>
           </TabPane>
           <TabPane tab="Ответы" key="2">
-            {answers
-              ? answers.map((answer: AnswerType) => (
-                  <Answer key={answer.id} data={answer} />
-                ))
-              : null}
+            {/*{answers*/}
+            {/*  ? Object.keys(answers).map((answer) => (*/}
+            {/*      <Answer key={answer.id} data={answer} />*/}
+            {/*    ))*/}
+            {/*  : <Empty/>}*/}
           </TabPane>
         </Tabs>
       </Row>
