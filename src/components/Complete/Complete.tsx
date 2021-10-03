72845ed57047190944255736a8f1eec302e75bbc
Artem Konkov <kxnkxv@yandex.ru>
2021-10-03 11:33:39 +0300

    Complete

--- src/components/Complete/Complete.tsx
+++ src/components/Complete/Complete.tsx
@@ -1,67 +1,52 @@
 import React, { useState } from "react";
-import { AutoComplete } from "antd";
-import { SelectProps } from "antd/es/select";
-
 import { Search } from "./styled";
-
-function getRandomInt(max: number, min: number = 0) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const searchResult = (query: string) =>
-  new Array(getRandomInt(5))
-    .join(".")
-    .split(".")
-    .map((_, idx) => {
-      const category = `${query}${idx}`;
-      return {
-        value: category,
-        label: (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <span>
-              Найдено {query} в{" "}
-              <a
-                href={`https://s.taobao.com/search?q=${query}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {category}
-              </a>
-            </span>
-            <span>{getRandomInt(200, 100)} результатов</span>
-          </div>
-        ),
-      };
-    });
+import axios from "axios";
+import { URL } from "../../constants/API";
+import Auth from "../../store/auth";
+import { removeDuplicates } from "../../utils/removeDuplicates";
+import { SuggQuestionType } from "../../pages/NewQuestion/NewQuestion";
+import { Card, Typography } from "antd";
+import { List } from "../../pages/Themes/styled";
+import { QuestionType } from "../../types/QuestionType";
+import ThemeItem from "../../pages/Themes/components/ThemeItem/ThemeItem";
 
 const Complete: React.FC = () => {
-  const [options, setOptions] = useState<SelectProps<object>["options"]>([]);
-
-  const handleSearch = (value: string) => {
-    setOptions(value ? searchResult(value) : []);
+  const [similar, setSimilar] = useState<any[]>([]);
+  const temp: any[] = [];
+  const getSimilar = (e: any) => {
+    if (e.target.value != "") {
+      axios
+        .request<SuggQuestionType>({
+          method: "get",
+          url: `${URL}/questions/search?q=${e.target.value}`,
+          headers: {
+            Authorization: `Bearer ${Auth.token.accessToken}`,
+          },
+        })
+        .then((response) => {
+          temp.push(response.data.data);
+        });
+      setSimilar(removeDuplicates(temp));
+    }
   };
-
-  const onSelect = (value: string) => {
-    console.log("onSelect", value);
-  };
-
   return (
-    <AutoComplete
-      style={{ width: "100%" }}
-      options={options}
-      onSelect={onSelect}
-      onSearch={handleSearch}
-      filterOption={(inputValue, option) =>
-        option!.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
-      }
-    >
-      <Search placeholder="Найти ответ на любой вопрос" enterButton />
-    </AutoComplete>
+    <>
+      <Search
+        placeholder="Найти ответ на любой вопрос"
+        onChange={getSimilar}
+        enterButton
+      />
+      {similar.length != 0 ? (
+        <Card>
+          <Typography.Title>Похожие вопросы</Typography.Title>
+          <List>
+            {similar.map((question: QuestionType) => (
+              <ThemeItem question={question} />
+            ))}
+          </List>
+        </Card>
+      ) : null}
+    </>
   );
 };
 
